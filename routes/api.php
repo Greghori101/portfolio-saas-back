0431f79f4e0bca5e31d9630dbffb28189e6e46f6
Greghori101 <e.souala@esi-sba.dz>
2025-06-02 01:14:17 +0100

    add v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\PortfolioController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\PortfolioController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('portfolios', PortfolioController::class);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::apiResource('users', UserController::class);
+        Route::apiResource('portfolios', PortfolioController::class);
+    });
 });
