fca9314959461381238365c36a9a9ef3433b133b
Greghori101 <e.souala@esi-sba.dz>
2025-06-02 01:05:40 +0100

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PortfolioController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('portfolios', PortfolioController::class);
+});
