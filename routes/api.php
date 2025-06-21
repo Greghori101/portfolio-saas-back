4a489645ab0dbb220498ec3cbf99b64853d942ea
Greghori101 <e.souala@esi-sba.dz>
2025-06-21 23:14:16 +0100

    add controllers

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,20 @@
 
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\PortfolioController;
-use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\SubscriptionsController;
+use App\Http\Controllers\Api\V1\ThemesController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
     Route::middleware('auth:api')->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
         Route::post('/logout', [AuthController::class, 'logout']);
-        Route::apiResource('users', UserController::class);
         Route::apiResource('portfolios', PortfolioController::class);
+        Route::apiResource('themes', ThemesController::class);
+        Route::apiResource('subscriptions', SubscriptionsController::class);
     });
 });
